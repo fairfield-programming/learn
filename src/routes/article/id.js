5526ea39fd9e88b990a7a5f2bf26190bc3a29bd5
William McGonagle <wpmcgonagle@gmail.com>
2022-04-13 09:52:53 -0400

    Added Article Endpoint

--- src/routes/article/id.js
+++ src/routes/article/id.js
@@ -1,5 +1,21 @@
 module.exports = (req, res) => {
 
-    return res.json({});
+    if (req.params.id == undefined) return res.status(400).json({ error: "Not All Parameters Provided" });
+
+    article.findById(req.params.id).then((data) => {
+
+        if (data == null || data == undefined) return res.status(404).json({ error: "Not Found." });
+        if (verified == false) return res.status(403).json({ error: "Not Verified Yet." });
+
+        return res.json(data);
+
+    }).catch((error) => {
+
+        console.log(error);
+        return res.status(500).json({
+            error: "Internal Server Error."
+        });
+
+    });
 
 }
