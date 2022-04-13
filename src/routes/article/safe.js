a2cd6cc5a1de6281277cea176b04a0876934b2fb
William McGonagle <wpmcgonagle@gmail.com>
2022-04-13 10:03:15 -0400

    Fixed even stupider mistakes

--- src/routes/article/safe.js
+++ src/routes/article/safe.js
@@ -3,7 +3,9 @@ module.exports = (req, res) => {
     if (req.params.id == undefined) return res.status(400).json({ error: "Not All Parameters Provided" });
 
     Article.findOne({
-        where: req.params.id
+        where: { 
+            id: req.params.id
+        }
     }).then((data) => {
 
         if (data == null || data == undefined) return res.status(404).json({ error: "Not Found." });
