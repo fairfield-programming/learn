494cf6a3f5fe3d51f067539b582bea5161366778
William McGonagle <wpmcgonagle@gmail.com>
2022-04-13 08:59:31 -0400

    Building New Stuffs

--- src/index.js
+++ src/index.js
@@ -16,20 +16,7 @@ app.use(require("cors")({ origin: "*" }));
 // Endpoints
 app.get("/", require('./routes/index'));
 
-app.get("/question", require('./routes/question/index'));
-app.get("/question/recent", require('./routes/question/recent'));
-app.get("/question/unanswered", require('./routes/question/unanswered'));
-
-app.get("/question/:id/", require('./routes/question/id'));
-app.post("/question/:id/answer", require('./routes/answer/create'));
-app.get("/question/:id/related", require('./routes/question/related'));
-
-app.post("/question/post", require('./routes/question/create'));
-
-app.get("/answer/:id/", require('./routes/answer/id'));
-
-app.get("/user/:id/questions", require('./routes/user/questions'));
-app.get("/user/:id/answers", require('./routes/user/answers'));
+app.get("/article/:id", require("./routes/article/id"));
 
 // Sync the Database
 (async () => {
