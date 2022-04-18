7a4c4111a47116acb793c962a55c92c518778168
William McGonagle <wpmcgonagle@gmail.com>
2022-04-18 19:00:47 -0400

    Reformatted Array Structuring

--- src/routes/utility/checkText.js
+++ src/routes/utility/checkText.js
@@ -5,7 +5,10 @@ module.exports = (req, res) => {
 
     if (req.body.text == undefined) return res.status(400).json({ error: "Not All Parameters Provided." });
 
-    const scope = [ ...universal.generalUniversalDesignCheck(req.body.text), ...grammar.generalGrammarCheck(req.body.text) ];
+    const scope = [ 
+        ...universal.generalUniversalDesignCheck(req.body.text), 
+        ...grammar.generalGrammarCheck(req.body.text) 
+    ];
 
     return res.status(200).json({
         text: req.body.text,
